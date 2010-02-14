e01445b1bd56d69ec94ec7513296f9c5a3e56481
Eduard Litau <elitau@elitau.(none)>
2010-02-14 06:06:32 +0100

    ADDED initial layout from yaml.de

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,6 @@ class List < ActiveRecord::Base
   has_and_belongs_to_many :lists, 
     :join_table => "list_to_list", 
     :foreign_key => "left_list_id", 
-    :association_foreign_key => "right_list_id",
+    :association_foreign_key => "right_list_id"
     
 end
